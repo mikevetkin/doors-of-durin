a3f9bdaeb7028bca30dd4188f5c066357eb2f0ae
Michael Vetkin <mikevetkin@gmail.com>
2024-03-17 00:06:25 +0300

    Layout

--- src/context/moria/feature/doors-of-durin/ui/pages/DoorsOfDurin/index.tsx
+++ src/context/moria/feature/doors-of-durin/ui/pages/DoorsOfDurin/index.tsx
@@ -7,15 +7,19 @@ export const DoorsOfDurinPage = () => {
   const { viewState, onChange } = useDoorsOfDurin();
 
   return (
-    <div>
-      <Ithildin viewState={viewState.ithildin}>Something</Ithildin>
-      <Input
-        value={viewState.codeInput.value}
-        onChange={(el) => onChange(el.target.value)}
-      />
-      <Button disabled={viewState.enterButton.disabled}>
-        {viewState.enterButton.label}
-      </Button>
+    <div className="flex h-[100vh] items-center justify-center">
+      <div className="flex flex-col gap-4">
+        <Ithildin viewState={viewState.ithildin}>Something</Ithildin>
+        <div className="flex gap-2">
+          <Input
+            value={viewState.codeInput.value}
+            onChange={(el) => onChange(el.target.value)}
+          />
+          <Button variant="secondary" disabled={viewState.enterButton.disabled}>
+            {viewState.enterButton.label}
+          </Button>
+        </div>
+      </div>
     </div>
   );
 };
