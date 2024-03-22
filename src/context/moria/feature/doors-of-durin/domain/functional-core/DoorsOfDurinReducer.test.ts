e3655897e2e27c9bd2875753fb57510efb3c393a
Michael <onevetka@gmail.com>
2024-03-23 01:36:12 +0300

    Added tests

--- src/context/moria/feature/doors-of-durin/domain/functional-core/DoorsOfDurinReducer.test.ts
+++ src/context/moria/feature/doors-of-durin/domain/functional-core/DoorsOfDurinReducer.test.ts
@@ -1,11 +1,29 @@
+import { Friend } from '@/context/moria/feature/doors-of-durin/domain/entity/Friend';
 import { doorsOfDurinReducer } from './DoorsOfDurinReducer';
 import { doorsOfDurinState } from './DoorsOfDurinState';
+import { Stranger } from '@/context/moria/feature/doors-of-durin/domain/entity/Stranger';
 
 /**
  * Какие требования должна реализовать система
  */
 describe('Doors of Durin Events', () => {
-  describe('SayCodeEvent - сказать код', () => {});
+  describe('SayCodeEvent - сказать код', () => {
+    test('Если сказан верный код, то это друг', () => {
+      const state = doorsOfDurinReducer(doorsOfDurinState(), {
+        type: 'SayCodeEvent',
+        value: 'mellon',
+      });
+      expect(state.traveller).toBeInstanceOf(Friend);
+    });
+
+    test('Если верный код не сказан, то это чужак', () => {
+      const state = doorsOfDurinReducer(doorsOfDurinState(), {
+        type: 'SayCodeEvent',
+        value: 'abra kadabra',
+      });
+      expect(state.traveller).toBeInstanceOf(Stranger);
+    });
+  });
   describe('MoonBeganShineEvent - начинает светить луна', () => {
     const state = doorsOfDurinReducer(doorsOfDurinState(), {
       type: 'MoonBeganShineEvent',
