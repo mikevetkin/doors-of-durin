ec68ffd824e610b67d02d1b7f7a662aeff3473ba
Michael Vetkin <mikevetkin@gmail.com>
2024-03-23 20:19:51 +0300

    Added type

--- src/context/moria/feature/doors-of-durin/domain/functional-core/DoorsOfDurinReducer.ts
+++ src/context/moria/feature/doors-of-durin/domain/functional-core/DoorsOfDurinReducer.ts
@@ -2,7 +2,6 @@ import { CorrectCode } from '../entity/Code';
 import { Friend } from '../entity/Friend';
 import { Name } from '../entity/Name';
 import { Stranger } from '../entity/Stranger';
-import { Traveller } from '../entity/Traveller';
 import { DoorsOfDurinEvent as Event, SayCodeEvent } from './DoorsOfDurinEvent';
 import { DoorsOfDurinState as State } from './DoorsOfDurinState';
 
@@ -22,7 +21,7 @@ function sayCode(state: State, event: SayCodeEvent): State {
   const codeResult = CorrectCode.parse(event.value.toLowerCase());
   const nameResult = Name.parse('Frodo');
 
-  const traveller: Traveller =
+  const traveller =
     codeResult.isOk && nameResult.isOk
       ? new Friend({ code: codeResult.value, name: nameResult.value })
       : new Stranger();
