9e98bc132c3befdb240556cc1d40824a30999243
Michael <onevetka@gmail.com>
2024-03-26 02:40:36 +0300

    Fixed text

--- src/context/moria/feature/doors-of-durin/ui/pages/WelcomeToMoria/WelcomeToMoriaPresentation.ts
+++ src/context/moria/feature/doors-of-durin/ui/pages/WelcomeToMoria/WelcomeToMoriaPresentation.ts
@@ -3,12 +3,12 @@ import { WelcomeToMoriaViewState } from './WelcomeToMoriaViewState';
 
 const mapGreeting = {
   mellon: 'Welcome to Moria',
-  мэллон: 'Добро пожаловать в Морию',
+  мэллон: 'Добро пожаловать в Морию,',
 };
 
 export const welcomeToMoriaPresentation = (
   friend: Friend
 ): WelcomeToMoriaViewState =>
   new WelcomeToMoriaViewState({
-    greeting: `${mapGreeting[friend.code.string]}, ${friend.name.string}`,
+    greeting: `${mapGreeting[friend.code.string]} ${friend.name.string}`,
   });
