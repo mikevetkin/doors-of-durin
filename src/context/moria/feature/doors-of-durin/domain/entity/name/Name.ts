c79bf7db8512901d12605e8b75653fc02871c5b3
Michael <onevetka@gmail.com>
2024-04-02 11:50:47 +0300

    Deleted try catch from name

--- src/context/moria/feature/doors-of-durin/domain/entity/name/Name.ts
+++ src/context/moria/feature/doors-of-durin/domain/entity/name/Name.ts
@@ -5,14 +5,10 @@ export class Name {
   public string: string;
 
   static parse(rawName: string): Result<Name, AppError> {
-    try {
-      if (rawName.length === 0) throw NameError.ToShort;
-      if (rawName.length > 20) throw NameError.ToLong;
+    if (rawName.length === 0) return Result.err(NameError.TooShort);
+    if (rawName.length > 20) return Result.err(NameError.TooLong);
 
-      return ok(new Name(rawName));
-    } catch (error) {
-      return Result.err(error as AppError);
-    }
+    return ok(new Name(rawName));
   }
 
   private constructor(string: string) {
@@ -25,12 +21,12 @@ export const name = (): Name => {
 };
 
 const NameError = {
-  ToShort: new AppError({
+  TooShort: new AppError({
     code: 'ERROR_GETTING_NAME_INSTANCE',
     message: 'Слишком короткое имя',
     path: [],
   }),
-  ToLong: new AppError({
+  TooLong: new AppError({
     code: 'ERROR_GETTING_NAME_INSTANCE',
     message: 'Мы сомневаемся, что это имя',
     path: [],
